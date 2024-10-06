5ee531761fb8395a170de9752daf99dade7a0070
jyangch <jyang.0086@gmail.com>
2024-10-06 15:28:57 +0800

    update

--- heapy/data/xselect.py
+++ heapy/data/xselect.py
@@ -513,7 +513,7 @@ class epXselect(object):
         json.dump(fig.to_dict(), open(savepath + '/cum_lc.json', 'w'), indent=4, cls=NpEncoder)
         
         
-    def calculate_txx(self, sigma=3, mp=True, xx=0.9, pstart=None, pstop=None, savepath='./curve/duration'):
+    def calculate_txx(self, sigma=3, mp=True, xx=0.9, pstart=None, pstop=None, tbkg=None, savepath='./curve/duration'):
             
         savepath = os.path.abspath(savepath)
         
@@ -524,7 +524,7 @@ class epXselect(object):
         
         txx = ppTxx(self.src_ts, self.bkg_ts, self.lc_bins, self.regratio)
         txx.findpulse(sigma=sigma, mp=mp)
-        txx.accumcts(xx=xx, pstart=pstart, pstop=pstop)
+        txx.accumcts(xx=xx, pstart=pstart, pstop=pstop, tbkg=tbkg)
         txx.save(savepath=savepath)
 
 
